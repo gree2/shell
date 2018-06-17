1e2fabcba7779ae939fe0c022293bcf7d9c171c9
gree2 <hqlgree2@gmail.com>
2018-06-17 22:23:24 +0800

    扫描子网

---
+++ nmap/subnet.sh
@@ -0,0 +1,16 @@
+# ###############################################
+# 1. scan subnet
+# ###############################################
+nmap -T5 -n -sn 192.168.0.1/24
+# ###############################################
+    # Starting Nmap 7.31 ( https://nmap.org ) at 2018-06-17 13:22 CST
+    # Nmap scan report for 192.168.0.101
+    # Host is up (0.00064s latency).
+    # Nmap scan report for 192.168.0.103
+    # Host is up (0.022s latency).
+    # Nmap scan report for 192.168.0.104
+    # Host is up (0.011s latency).
+    # Nmap scan report for 192.168.0.253
+    # Host is up (0.018s latency).
+    # Nmap done: 256 IP addresses (4 hosts up) scanned in 3.96 seconds
+    # ###############################################
